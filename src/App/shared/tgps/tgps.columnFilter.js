14a7d7c3e707e94fa6874377fb1ac55abf50c901
naruto1106 <naruto991223@gmail.com>
2022-07-19 17:52:33 +0530

    #4 To add filters - Fully done, Product list show columns popup added for view selected column only - Missing files added

---
+++ src/App/shared/tgps/tgps.columnFilter.js
@@ -0,0 +1,31 @@
+﻿agmNgModuleWrapper("agms.tgps")
+    .defineControllerAsPopup("s.tgps.ColumnFilterController",
+        {
+            templateUrl: '/App/shared/tgps/tgps.columnFilter.html',
+            windowClass: 'default-modal tgps-fundamental-filter-popup'
+        },
+        ['productListOptionForPosition'],
+        function (vm, dep, tool) {
+            
+            function closePanel() {
+                vm.uibClosePanel(dep.productListOptionForPosition);
+            }
+
+            function onChange(item) {                                
+                var isNoneChecked = _.all(dep.productListOptionForPosition.columns, function(i) {
+                    return !i.checked;
+                });
+
+                if (isNoneChecked) {
+                    item.checked = true;
+                }
+            }
+
+            tool.initialize(function () {
+                tool.setVmProperties({
+                    closePanel:closePanel,
+                    onChange: onChange,
+                    productListOptionForPosition: dep.productListOptionForPosition                    
+                });
+            });
+        });
