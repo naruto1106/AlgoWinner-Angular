0151f81626c5ca90cc8f2a28ed3f92ec67eea7da
naruto1106 <naruto991223@gmail.com>
2022-08-24 19:23:03 +0530

    Risk Analyzer all UI completed including Construct Portfolio popup, inline css converted in to css class

--- src/App/shared/tgps/tgps.riskAnalyzer.js
+++ src/App/shared/tgps/tgps.riskAnalyzer.js
@@ -2,7 +2,7 @@
     .defineControllerAsPopup("s.tgps.RiskAnalyzerController",
         {
             templateUrl: '/App/shared/tgps/tgps.riskAnalyzer.html',
-            windowClass: 'default-modal tgps-fundamental-filter-popup tgps-fundamental-filter-risk-analyze-popup'
+            windowClass: 'default-modal tgps-risk-analyze-popup'
         }, ['mode'],
         function (vm, dep, tool) {
             
@@ -34,29 +34,113 @@
             function disabled(param) {
                 return (param.mode === 'day' && (param.date.getDay() === 0 || param.date.getDay() === 6));
             }
+
+            function submitStockRisk(){
+
+            }
+            function loadConstructPortfolio() {
+                tool.openModalByDefinition('s.tgps.riskAnalyzerConstructPortfolioController', {
+                    mode: vm.mode
+                });
+            }
+            function submitPortfolioRisk(){
+
+            }
+            function submitMomentumProfiler(){
+
+            }
+            function submitTradeSizing(){
+
+            }
             
             tool.initialize(function () {
                 tool.setVmProperties({
                     closePanel:closePanel,
                     currentTab: 'stock_risk',
-                    setTab: setTab,                    
+                    setTab: setTab, 
+                    submitStockRisk: submitStockRisk,
+                    loadConstructPortfolio: loadConstructPortfolio,
+                    submitPortfolioRisk: submitPortfolioRisk,
+                    submitMomentumProfiler: submitMomentumProfiler,
+                    submitTradeSizing: submitTradeSizing,
                     stock_risk: {
+                        symbol: '',
+                        benchmark: '',
                         analysisDate: '',
+                        lookback_horizon: '',
                         dateOpened: false,
                         dateSelectionMode: 0,
                         setStockRiskAnalysisDateSelectionOpen: setStockRiskAnalysisDateSelectionOpen,
+                        table: [
+                            {
+                                name: 'Correlation',
+                                apple: '0.2',
+                                spy: '-',
+                            },
+                            {
+                                name: 'Beta',
+                                apple: '1.1',
+                                spy: '-',
+                            },
+                            {
+                                name: 'Sharpe Ratio',
+                                apple: '0.8	',
+                                spy: '0.4',
+                            },
+                            {
+                                name: 'Return',
+                                apple: '25%',
+                                spy: '16%',
+                            }
+                        ],
+                        volacity: 27
                     },
                     portfolio_risk: {
+                        benchmark: '',
                         analysisDate: '',
+                        lookback_horizon: '',
                         dateOpened: false,
                         dateSelectionMode: 0,
                         setPortfolioRiskAnalysisDateSelectionOpen: setPortfolioRiskAnalysisDateSelectionOpen,
+                        table: [
+                            {
+                                name: 'Correlation',
+                                apple: '0.2',
+                                spy: '-',
+                            },
+                            {
+                                name: 'Beta',
+                                apple: '1.1',
+                                spy: '-',
+                            },
+                            {
+                                name: 'Sharpe Ratio',
+                                apple: '0.8	',
+                                spy: '0.4',
+                            },
+                            {
+                                name: 'Return',
+                                apple: '25%',
+                                spy: '16%',
+                            }
+                        ],
+                        volacity: 27
                     },
                     momentum_profiler: {
+                        symbol: '',
+                        direction: '',
                         analysisDate: '',
+                        lookback_horizon: '',
                         dateOpened: false,
                         dateSelectionMode: 0,
                         setMomentumProfilerAnalysisDateSelectionOpen: setMomentumProfilerAnalysisDateSelectionOpen,
+                        momentum: 25,
+                        contrarian: 75
+                    },
+                    trade_sizing: {
+                        symbol: '',
+                        regular_size: '',
+                        trade_size: 2
                     },
                     dateOptions: {
                         formatYear: 'yyyy',
---
+++ src/App/shared/tgps/tgps.riskAnalyzerConstructPortfolio.js
@@ -0,0 +1,38 @@
+﻿agmNgModuleWrapper("agms.tgps")
+    .defineControllerAsPopup("s.tgps.riskAnalyzerConstructPortfolioController",
+        {
+            templateUrl: '/App/shared/tgps/tgps.riskAnalyzerConstructPortfolio.html',
+            windowClass: 'default-modal tgps-risk-analyze-popup tgps-risk-analyze-construct-portfolio-popup'
+        }, ['mode'],
+        function (vm, dep, tool) {
+            
+            vm.mode = dep.mode;            
+
+            function closePanel() {
+                vm.uibClosePanel();
+            }
+            
+            
+            function addRow(){
+
+            }
+            function removeRow(index){
+
+            }
+            
+            tool.initialize(function () {
+                tool.setVmProperties({
+                    closePanel:closePanel,
+                    constructPortfolios: [
+                        {
+                            id: 1,
+                            data: [
+                                {
+                                    
+                                }
+                            ]
+                        }
+                    ]
+                });
+            });
+        });
