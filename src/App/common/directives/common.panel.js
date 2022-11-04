ed433a4ccea0cc45733d27d84db538a1dc90e161
naruto1106 <naruto991223@gmail.com>
2022-11-04 12:28:16 +0530

    worked on ui

--- src/App/common/directives/common.panel.js
+++ src/App/common/directives/common.panel.js
@@ -16,8 +16,27 @@
 
         });
 
-        $('.bar_icon').click(function(){
-            debugger
-            $('.static-sidebar').removeClass('test')
-            $(this).addClass('test');
-        });
+
+
+
+        function toggleItem(elem) {
+            for (var i = 0; i < elem.length; i++) {
+              elem[i].addEventListener("click", function(e) {
+                var current = this;
+                for (var i = 0; i < elem.length; i++) {
+                  if (current != elem[i]) {
+                    elem[i].classList.remove('active');
+                  } else if (current.classList.contains('active') === true) {
+                    current.classList.remove('active');
+                  } else {
+                    current.classList.add('active')
+                  }
+                }
+                e.preventDefault();
+              });
+            };
+          }
+          toggleItem(document.querySelectorAll('.link'));
+
+
+          
