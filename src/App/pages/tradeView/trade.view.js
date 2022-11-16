ddb4438e049abcf2fdd8593b8912c2f98a495939
naruto1106 <naruto991223@gmail.com>
2022-11-16 18:59:21 +0530

    worked on trade page

---
+++ src/App/pages/tradeView/trade.view.js
@@ -0,0 +1,17 @@
+agmNgModuleWrapper('agmp.tradeview')
+    .defineController('p.tradeView.TradeViewController',
+        function (vm, dep, tool) {
+            
+
+        
+        }) 
+        .defineDirectiveForE('agmp-TradeComponent-simple-screener', [],
+        function () {
+            return {
+                controller: "p.tradeView.TradeViewController",
+                templateUrl: '/App/pages/tradeView/trade.view.html'
+            };
+        },
+        {
+
+        });
---
+++ src/App/pages/tradeView/tradeview._module.js
@@ -0,0 +1,5 @@
+agmNgModuleWrapper('agmp.tradeview',
+[
+
+   
+   ]);
