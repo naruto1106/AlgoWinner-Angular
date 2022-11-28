91754e72e8dec37643fd4037efe305dc1379c73b
naruto1106 <naruto991223@gmail.com>
2022-11-28 15:06:29 +0530

    changes

--- src/App/pages/tradeIdea/tradeIdea.main.js
+++ src/App/pages/tradeIdea/tradeIdea.main.js
@@ -2,8 +2,10 @@ agmNgModuleWrapper('agmp.tradeIdea')
     .defineController('p.tradeIdea.MainController',['$scope'],
         function ($scope) {
             var vm = this;
+            $scope.selectedFilter = '';
             $scope.selectedTab = 'technical';
-            $scope.selectedAccordin ='';
+            $scope.selectedAccordin ='basic';
+            $scope.techBarValue1 ={'id':'4'};
             $scope.selectedPeriod ={};
             $scope.isprocessing = false;
             $scope.periodText = { buttonDefaultText: 'Choose Period'};
@@ -11,19 +13,36 @@ agmNgModuleWrapper('agmp.tradeIdea')
                 console.log("set processing True;")
                 $scope.isprocessing = true;
             }
-            $scope.changeTab=  function (tab) {
-                console.log("Selected Tab:",tab)
+            $scope.changeTab=  function (tab,accordion) {
+                console.log("Set Tab:",tab)
+                console.log("Set Accordion:",accordion)
                 $scope.selectedTab=tab;
+                $scope.selectedAccordin = accordion;
             }
             $scope.changeTabAccordion=  function (tab,accordion) {
-                console.log("Selected Accordion:",tab)
+                console.log("Selected Accordion:",accordion)
                 $scope.selectedAccordin = accordion;
+            }
+            $scope.setTabFilter=  function (filter) {
+                console.log("set Filter is:",filter)
+                $scope.selectedFilter = filter;
             }
 
             $scope.data = {
                 Periods: [
                     { id: '1', name: 'Annual' },
                     { id: '2', name: 'Quartal' },
+                ],
+                periodTypes: [
+                    { id: '1', name: 'Days' },
+                    { id: '2', name: 'Week' },
+                ],
+                barPatternValue1: [
+                    { id: '1', name: 'Open' },
+                    { id: '2', name: 'High' },
+                    { id: '3', name: 'Low' },
+                    { id: '4', name: 'Close' },
+                    { id: '5', name: 'Volume' },
                 ],
             }
 
