48146b9df69c6ae9ec7ae846b654fb342b63d7b0
naruto1106 <naruto991223@gmail.com>
2022-12-02 19:17:56 +0530

    Worked on Fundamental, Sentiment and Event Tab

--- src/App/pages/tradeIdea/tradeIdea.main.js
+++ src/App/pages/tradeIdea/tradeIdea.main.js
@@ -58,6 +58,22 @@ agmNgModuleWrapper('agmp.tradeIdea')
             $scope.normOBValueB={};
             $scope.majorPriceBrktPrd={};
             $scope.peakTroughType ={};
+            $scope.fundPeriod ={};
+            $scope.fGMPeriod ={};
+            $scope.fEGPeriod ={};
+            $scope.benchmarkRPValue =[];
+            $scope.benchmarkRPPeriod ={};
+            $scope.benchmarkREarnperiod ={};
+            $scope.benchmarkREarnVal =[];
+            $scope.catMarketCapVal ={};
+            $scope.analystRating ={};
+            $scope.latestNewsSentiment ={};
+            $scope.latestNewsRange ={};
+            $scope.analystTargetP1 ={};
+            $scope.analystTargetP2 ={};
+            $scope.analystActionType ={};
+            $scope.breakoutMulti ={};
+            $scope.unusualGapType ={};
 
             $scope.selectedPeriod ={};
             $scope.lookbackPeriod ={};
@@ -78,8 +94,8 @@ agmNgModuleWrapper('agmp.tradeIdea')
                 $scope.isprocessing = true;
             }
             $scope.changeTab=  function (tab) {
-                console.log("Set Tab:",tab)
                 $scope.selectedTab=tab;
+                $scope.selectedFilter = '';
             }
             $scope.changeTabAccordion=  function (accordion) {
                 console.log("Selected Accordion:",accordion)
@@ -95,6 +111,11 @@ agmNgModuleWrapper('agmp.tradeIdea')
                 Periods: [
                     { id: '1', name: 'Annual' },
                     { id: '2', name: 'Quartal' },
+                ],
+                yearPeriods: [
+                    { id: 1, name: '1Y' },
+                    { id: 2, name: '3Y' },
+                    { id: 3, name: '5Y' },
                 ],
                 periodTypes: [
                     { id: '1', name: 'Days' },
@@ -196,13 +217,56 @@ agmNgModuleWrapper('agmp.tradeIdea')
                     { id: '4', name: '78.6%'},
                 ],
                 confirmation: [
-                    { id: '1', name: 'Yes' },
-                    { id: '2', name: 'No' },
+                    { id: 1, name: 'Yes' },
+                    { id: 2, name: 'No' },
                 ],
                 trianglePatternType: [
                     { id: 1, name: 'Symmetrical' },
                     { id: 2, name: 'Ascending' },
                     { id: 3, name: 'Descending' },
+                ],
+                catMarketCapVal: [
+                    { id: 1, name: '< 500M' },
+                    { id: 2, name: '500M - 1B' },
+                    { id: 3, name: '1B - 5B' },
+                    { id: 4, name: '> 5B' },
+                ],
+                analystRatingVal: [
+                    { id: 1, name: '1 to 1.5 (strong buy)' },
+                    { id: 2, name: '1.5 to 2 (buy)' },
+                    { id: 3, name: '2 to 2.5 (buy)' },
+                    { id: 4, name: '3 to 3.5 (hold)' },
+                    { id: 5, name: '3.5 to 4 (sell)' },
+                    { id: 6, name: '4 to 4(sell)' },
+                    { id: 7, name: '4.5 to 5 (strong sell)' },
+                ],
+                latestNewsSentiment: [
+                    { id: 1, name: 'Positive' },
+                    { id: 2, name: 'Negative' },
+                    { id: 3, name: 'Neutral' }
+                ],
+                latestNewsRange: [
+                    { id: 1, name: '1 day' },
+                    { id: 2, name: '2 days' },
+                    { id: 3, name: '3 days' }
+                ],
+                analystTargetP2: [
+                    { id: 1, name: '< 10%' },
+                    { id: 2, name: '< 20%' },
+                    { id: 3, name: '20% to 50%' },
+                    { id: 4, name: '> 50%' }
+                ],
+                analystActionType: [
+                    { id: 1, name: 'Upgrade Rating' },
+                    { id: 2, name: 'Downgrade Rating' },
+                    { id: 3, name: 'Upgrade Earning Forecast' },
+                    { id: 4, name: 'Downgrade Earning Forecast' }
+                ],
+                breakoutMulti: [
+                    { id: 1, name: '< 25%' },
+                    { id: 2, name: '< 50%' },
+                    { id: 3, name: '50% to 75%' },
+                    { id: 4, name: '>100%' }
                 ],
             }
 
