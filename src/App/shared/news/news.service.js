0def5433f444bfddc4a3a8ddc45c5adb4b2757a1
naruto1106 <naruto991223@gmail.com>
2023-07-31 19:11:54 +0700

    adding upcoming earning

--- src/App/shared/news/news.service.js
+++ src/App/shared/news/news.service.js
@@ -1,25 +1,32 @@
 ﻿agmNgModuleWrapper("agms.news")
     .defineService("sNewsService", [],
-    function (serviceObj, dep, tool) {
-        var coreServerCommunicationService = dep.coreServerCommunicationService;
-        var path = '/newsapi/v1/News/';
+        function (serviceObj, dep, tool) {
+            var coreServerCommunicationService = dep.coreServerCommunicationService;
+            var path = '/newsapi/v1/News/';
+            var EarningMSPath = '/earningms/v1/Earning/'
 
-        tool.setServiceObjectProperties({
-            AddAlgoNews: coreServerCommunicationService.genPostFunction(path + 'AddAlgoNews'),
-            GetAlgoNewsByFilter: coreServerCommunicationService.genPostFunction(path + 'GetAlgoNewsByFilterV2'),
-            GetBreakingNews: coreServerCommunicationService.genGetFunctionWithNVar(path + 'GetBreakingNews',
-                function (args) {
-                    return {
-                        market: args[0]
-                    };
-                }),
-            GetRelatedNews: coreServerCommunicationService.genPostFunction(path + 'GetRelatedNews'),
-            GetNewsForProductPage: coreServerCommunicationService.genGetFunctionWithNVar(path + 'GetNewsForProductPage',
-                function (args) {
-                    return {
-                        productId: args[0],
-                        market: args[1]
-                    };
-                })
+            tool.setServiceObjectProperties({
+                GetNextEarningDate: coreServerCommunicationService.genGetFunctionWithNVar(EarningMSPath + 'GetNextEarningDate',
+                    function (args) {
+                        return {
+                            symbol: args[0]
+                        };
+                    }),
+                AddAlgoNews: coreServerCommunicationService.genPostFunction(path + 'AddAlgoNews'),
+                GetAlgoNewsByFilter: coreServerCommunicationService.genPostFunction(path + 'GetAlgoNewsByFilterV2'),
+                GetBreakingNews: coreServerCommunicationService.genGetFunctionWithNVar(path + 'GetBreakingNews',
+                    function (args) {
+                        return {
+                            market: args[0]
+                        };
+                    }),
+                GetRelatedNews: coreServerCommunicationService.genPostFunction(path + 'GetRelatedNews'),
+                GetNewsForProductPage: coreServerCommunicationService.genGetFunctionWithNVar(path + 'GetNewsForProductPage',
+                    function (args) {
+                        return {
+                            productId: args[0],
+                            market: args[1]
+                        };
+                    })
+            });
         });
-    });
